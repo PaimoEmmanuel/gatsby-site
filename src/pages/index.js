4ca5d30af47ef1b14612fb51e868a64050c7dae0
Paimo Emmanuel <paimoemmanuel2016@gmail.com>
2020-09-23 14:21:12 +0100

    Cleaned up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ const IndexPage = () => {
         <Head title="Home"/>
         <h1>Hello</h1>
         <h2>I'm Paimo, a fullstack dev, living in Lagos</h2>
-        <p>Need a developer? <Link to="/contact">Contact me</Link></p>
       </Layout>
   )
 }
